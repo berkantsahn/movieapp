f8b6da7ca779669ed6e363d564dcf61e844accb2
berkantsahn <berkantsahn@gmail.com>
2022-11-25 10:59:13 +0300

    Added popular movies

--- src/app/models/movie.repository.ts
+++ src/app/models/movie.repository.ts
@@ -6,16 +6,20 @@ export class MovieRepository {
     //class içerisindeyse bir değişken tanımlarken boş olmasına karşın bir hata döndürür. Bunu engellemek için constructor tanımlamalıyız
     constructor(){
         this.movies = [
-            { id: 1, title: "Film 1", description: "Film 1 Açıklama", imageUrl: "1.jpeg"},
-            { id: 2, title: "Film 2", description: "Film 2 Açıklama", imageUrl: "2.jpeg"},
-            { id: 3, title: "Film 3", description: "Film 3 Açıklama", imageUrl: "3.jpeg"},
-            { id: 4, title: "Film 4", description: "Film 4 Açıklama", imageUrl: "4.jpeg"},
-            { id: 5, title: "Film 5", description: "Film 5 Açıklama", imageUrl: "5.jpeg"}
+            { id: 1, title: "Film 1", description: "Film 1 Açıklama", imageUrl: "1.jpeg", isPopular: false},
+            { id: 2, title: "Film 2", description: "Film 2 Açıklama", imageUrl: "2.jpeg", isPopular: true},
+            { id: 3, title: "Film 3", description: "Film 3 Açıklama", imageUrl: "3.jpeg", isPopular: true},
+            { id: 4, title: "Film 4", description: "Film 4 Açıklama", imageUrl: "4.jpeg", isPopular: true},
+            { id: 5, title: "Film 5", description: "Film 5 Açıklama", imageUrl: "5.jpeg", isPopular: false}
           ];
     }
 
     getMovies(): Movie[]{
         return this.movies;
+    }
+
+    getPopularMovies(): Movie[]{
+        return this.movies.filter(i => i.isPopular);
     }
 
     //hatayı engellemek için tsconfig.json'da lib'in altına "strictNullChecks": false komutunu ekliyoruz
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -9,12 +9,15 @@ import { MovieRepository } from '../models/movie.repository';
 })
 export class MoviesComponent implements OnInit {
 
+  title:string = "Film Listesi"
   movies: Movie[];
+  popularMovies: Movie[];
   movieRepository: MovieRepository;
 
   constructor(){
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
+    this.popularMovies = this.movieRepository.getPopularMovies();
   }
 
   ngOnInit(): void {
